1e0226df1868d5462dedb248b21a65b6a3113c21
canon4 <canondiego0@gmail.com>
2023-04-25 23:06:38 -0500

    se actualizo la clase carta

--- src/Carta.java
+++ src/Carta.java
@@ -1,3 +1,6 @@
+
+import javax.swing.ImageIcon;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,69 @@
  * @author Diego Canon
  */
 public class Carta {
+    private String nombre;
+    private double ataque;
+    private double defensa;
+    private String tipo;
+    private ImageIcon imagen;
+    private String descripcion;
+
+    public Carta(String nombre, double ataque, double defensa, String tipo, ImageIcon imagen, String descripcion) {
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.tipo = tipo;
+        this.imagen = imagen;
+        this.descripcion = descripcion;
+    }
     
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(double ataque) {
+        this.ataque = ataque;
+    }
+
+    public double getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(double defensa) {
+        this.defensa = defensa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public ImageIcon getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(ImageIcon imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
